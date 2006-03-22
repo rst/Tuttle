99a6f3d4815421a23613c69da46c83d694965d33
rst <>
2006-03-22 23:48:27 +0000

    New test framework for Tuttle, which should allow me to branch the thing without breaking the test suite on the trunk.  (I had been storing "known good" filesystem structures directly in CVS.  CVS sorta copes with files that exist only on a branch --- but *directories* that exist only on a branch just don't work.  If you try it, you'll find the directory showing up in fresh trunk checkouts --- but if the directory is empty, then you *won't* see it in a fresh checkout on the branch.  The fixes for this all involve upgrading to something which isn't CVS).

--- test/bin/test.pl
+++ test/bin/test.pl
@@ -5,16 +5,23 @@ use lib "$FindBin::Bin/../..";
 use Tuttle::Config;
 use Data::Dumper;
 use File::Path;
+use IO::File;
+use Digest::MD5;
+use strict;
 
-$sandbox_src = "$FindBin::Bin/../sandbox0/";
-$sandbox = "$FindBin::Bin/../tmp/sandbox";
-$base = "$sandbox/gold";
+my $testdata_dir = "$FindBin::Bin/../testdata/";
+my $sandbox = "$FindBin::Bin/../tmp/sandbox";
+my $base = "$sandbox/gold";
 
-rmtree $sandbox;
-system ("/usr/bin/rsync", "--checksum", "--delete", "-a",
-	$sandbox_src, $sandbox);
+# Have to set umask, to make new modes consistent with "freeze-dried"
+# regression tests.
+
+umask 022;
+&reconstitute_sandbox ($testdata_dir . 'sandbox0.gold');
 
 # A few error tests...
+
+my $c;
 
 eval {
   $c = Tuttle::Config->new ('looptest', "$base/looptest/Tuttle");
@@ -138,14 +145,100 @@ unlink "$sandbox/etc/packages";	# doesn't uninstall in real life
 
 sub install_test {
   my ($ref_name) = @_;
-  my $dir_name = "$FindBin::Bin/../$ref_name";
-  if (0 != system ("diff -qr --exclude=CVS --exclude=gold $dir_name $sandbox")) {
-    die "Unexpected differences with $dir_name";
+  my $good_data_locn = $testdata_dir . $ref_name;
+  my $arch_locn = "$FindBin::Bin/../tmp/$ref_name";
+
+  &freeze_dry_sandbox($arch_locn);
+
+  if (0 != system ("diff -u $good_data_locn $arch_locn")) {
+    die "Unexpected differences with $arch_locn";
   }
-  if (-d "$sandbox/var/spool/invoices/CVS" || 
+  if (-d "$sandbox/var/spool/invoices/CVS" ||
       -d "$sandbox/var/spool/invoices/client1/CVS")
   {
     die "'tree' directive copying CVS files"
   }
 }
 
+sub freeze_dry_sandbox {
+  my ($arch_locn) = @_;
+  my ($output) = new IO::File;
+  $output->open(">$arch_locn") or die "Couldn't open $arch_locn";
+  &freeze_dry_dir ($output, $sandbox, '.');
+  $output->close;
+}
+
+sub freeze_dry_dir {
+  my ($output, $full_path, $rel_path) = @_;
+
+  opendir (DIR, $full_path) or die "Couldn't read $full_path";
+  my @entries = readdir DIR;
+  closedir DIR;
+
+  @entries = sort (grep { $_ ne 'CVS' && $_ ne 'gold'
+			  && $_ ne '.' && $_ ne '..'
+			} @entries);
+
+  for my $entry (@entries) {
+    my $entry_full_path = "${full_path}/${entry}";
+    my $entry_rel_path  = "${rel_path}/${entry}";
+    my ($dev, $ino, $mode) = stat $entry_full_path;
+    my ($modestr) = sprintf "%o", ($mode & 0777);
+
+    if (-d $entry_full_path) {
+      print $output "==== $entry_rel_path d$modestr\n";
+      &freeze_dry_dir ($output, $entry_full_path, $entry_rel_path);
+    }
+    else {
+      # Presume ordinary file.
+      my ($chksum) = Digest::MD5->new;
+      my ($in) = new IO::File $entry_full_path, "r";
+      $chksum->addfile ($in);
+      $chksum = $chksum->b64digest;
+      $in->close;
+      print $output "==== $entry_rel_path $modestr $chksum\n";
+      $in = new IO::File $entry_full_path, "r";
+      print $output $in->getlines;
+      $in->close;
+    }
+  }
+}
+
+sub reconstitute_sandbox {
+  my ($archive) = @_;
+
+  rmtree $sandbox;
+  mkdir $sandbox or die "Couldn't create $sandbox";
+
+  open (IN, "<$archive") or die "Couldn't read $archive";
+  my ($current_output) = undef;
+
+  while (<IN>) {
+    if (! /^==== /) {
+      print $current_output $_;
+    }
+    else {
+      # Have a new header... current entry is done
+      if ($current_output) {
+	$current_output->close;
+	$current_output = undef;
+      }
+
+      if ($_ =~ /^==== ([^ ]+) d([0-7]+)/) {
+	# directory entry
+	mkdir "$sandbox/$1", oct($2) or die "Couldn't create $sandbox/$1";
+      }
+      elsif ($_ =~ /^==== ([^ ]+) ([0-7]+) (\S+)/) {
+	my $file = "$sandbox/$1";
+	$current_output = new IO::File $file, "w";
+	chmod oct($2), $file;
+	if (!$current_output) {
+	  die "Couldn't create $file";
+	}
+      }
+      else {
+	die "Garbled header $_";
+      }
+    }
+  }
+}
